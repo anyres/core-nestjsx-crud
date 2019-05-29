c842fd7e0e8572b755b4a44f30f16e4b6313de25
niuba <niuba.zbz@gmail.com>
2019-05-29 10:47:05 +0800

    fix version
    Signed-off-by: niuba <niuba.zbz@gmail.com>

--- src/AnyresNestjsxCRUD.ts
+++ src/AnyresNestjsxCRUD.ts
@@ -1,35 +1,34 @@
-
 import { AnyresCRUD } from "@anyres/core";
 import { Observable, throwError } from "rxjs";
 import { catchError, map, switchMap } from "rxjs/operators";
 
-export enum DilterConditions {
-  eq = 'eq', // (=, equal)
-  ne = 'ne', // (!=, not equal)
-  gt = 'gt', // (>, greater than)
-  lt = 'lt', // (<, lower that)
-  gte = 'gte', // (>=, greater than or equal)
-  lte = 'lte', // (<=, lower than or equal)
-  starts = 'starts', // (LIKE val%, starts with)
-  ends = 'ends', // (LIKE %val, ends with)
-  cont = 'cont', // (LIKE %val%, contains)
-  excl = 'excl', // (NOT LIKE %val%, not contains)
-  in = 'in', // (IN, in range, accepts multiple values)
-  notin = 'notin', // (NOT IN, not in range, accepts multiple values)
-  isnull = 'isnull', // (IS NULL, is NULL, doesn't accept value)
-  notnull = 'notnull', // (IS NOT NULL, not NULL, doesn't accept value)
-  between = 'between', // (BETWEEN, between, accepts two values)
+export enum FilterConditionEnum {
+  eq = "eq", // (=, equal)
+  ne = "ne", // (!=, not equal)
+  gt = "gt", // (>, greater than)
+  lt = "lt", // (<, lower that)
+  gte = "gte", // (>=, greater than or equal)
+  lte = "lte", // (<=, lower than or equal)
+  starts = "starts", // (LIKE val%, starts with)
+  ends = "ends", // (LIKE %val, ends with)
+  cont = "cont", // (LIKE %val%, contains)
+  excl = "excl", // (NOT LIKE %val%, not contains)
+  in = "in", // (IN, in range, accepts multiple values)
+  notin = "notin", // (NOT IN, not in range, accepts multiple values)
+  isnull = "isnull", // (IS NULL, is NULL, doesn't accept value)
+  notnull = "notnull", // (IS NOT NULL, not NULL, doesn't accept value)
+  between = "between" // (BETWEEN, between, accepts two values)
 }
 
 export interface IFilter<T> {
   field: T;
-  condition: DilterConditions;
-  value: string;
+  condition: FilterConditionEnum;
+  value: any;
 }
 
 export interface ISort<T> {
   field: T;
-  by: 'DESC' | 'ASC';
+  by: "DESC" | "ASC";
 }
 
 export interface IJoin {
@@ -39,9 +38,9 @@ export interface IJoin {
 
 export interface INestjsxResQuery<T> {
   fields?: Array<keyof T>;
-  filter?: IFilter<keyof T>[];
-  or?: IFilter<keyof T>[];
-  sort?: ISort<keyof T>[];
+  filter?: Array<IFilter<keyof T>>;
+  or?: Array<IFilter<keyof T>>;
+  sort?: Array<ISort<keyof T>>;
   join?: IJoin[];
   limit?: number;
   offset?: number;
@@ -50,33 +49,26 @@ export interface INestjsxResQuery<T> {
 }
 
 export interface INestjsxResQueryResult<T> {
+  count: number;
+  data: T[];
+  page: number;
+  pageCount: number;
   total: number;
-  list: T[];
 }
 
-export interface INestjsxResGet {
-}
+export interface INestjsxResGet {}
 
-export interface INestjsxResCreate {
-}
+export interface INestjsxResCreate {}
 
-export interface INestjsxResUpdate {
-}
-
+export interface INestjsxResUpdate {}
 
 export class AnyresNestjsxCRUD<
   TQ extends INestjsxResQuery<TG>,
   TG extends INestjsxResGet,
   TC extends INestjsxResCreate,
   TU extends INestjsxResUpdate
-  > extends AnyresCRUD<
-  TQ,
-  INestjsxResQueryResult<TG>,
-  TG,
-  TC,
-  TU
-  > {
-  formatFields(query: TQ): string {
+> extends AnyresCRUD<TQ, INestjsxResQueryResult<TG>, TG, TC, TU> {
+  public formatFields<T>(query: { fields?: Array<keyof T> }): string {
     if (query && query.fields) {
       return `fields=${query.fields.join(",")}`;
     } else {
@@ -84,84 +76,117 @@ export class AnyresNestjsxCRUD<
     }
   }
 
-  formatFilter(query: TQ) {
+  public formatFilter<T>(query: { filter?: Array<IFilter<keyof T>> }) {
     if (query && query.filter) {
-      return query.filter.map(filter => `filter=${filter.field}||${filter.condition}||${filter.value}`).join('&')
+      return query.filter
+        .map(
+          filter =>
+            `filter=${filter.field}||${filter.condition}||${filter.value}`
+        )
+        .join("&");
     } else {
       return "";
     }
   }
-  formatOr(query: TQ) {
+  public formatOr<T>(query: { or?: Array<IFilter<keyof T>> }) {
     if (query && query.or) {
-      return query.or.map(or => `or=${or.field}||${or.condition}||${or.value}`).join('&')
+      return query.or
+        .map(or => `or=${or.field}||${or.condition}||${or.value}`)
+        .join("&");
     } else {
       return "";
     }
   }
-  formatSort(query: TQ) {
+  public formatSort<T>(query: { sort?: Array<ISort<keyof T>> }) {
     if (query && query.sort) {
-      return query.sort.map(sort => `sort=${sort.field},${sort.by}`).join('&')
+      return query.sort.map(sort => `sort=${sort.field},${sort.by}`).join("&");
     } else {
       return "";
     }
   }
-  formatJoin(query: TQ) {
+  public formatJoin(query: { join?: IJoin[] }) {
     if (query && query.join) {
-      return query.join.map(join => {
-        if (join.fields) {
-          return `join=${join.relation}||${join.fields.join(",")}`
-        } else {
-          return `join=${join.relation}`
-        }
-      }).join('&')
+      return query.join
+        .map(join => {
+          if (join.fields) {
+            return `join=${join.relation}||${join.fields.join(",")}`;
+          } else {
+            return `join=${join.relation}`;
+          }
+        })
+        .join("&");
     } else {
       return "";
     }
   }
-  public get(id: string | number, query?: TQ): Observable<TG> {
+  public createMany(res: TC[]): Observable<TG[]> {
     return this.getHeaders$().pipe(
-      switchMap((headers) => {
-        return this.httpAdapter.get(`${this.path}/${id}?${this.formatFields(query)}&${this.formatJoin(query)}`, {
-          headers,
+      switchMap(headers => {
+        return this.httpAdapter.post(`${this.path}/bulk`, {
+          body: {
+            bulk: res
+          },
+          headers
         });
       }),
-      map((response) => response.json() as TG),
+      map(response => response.json() as TG[]),
       catchError((err: any) => {
         this.errorHandler(err);
         return throwError(err);
+      })
+    );
+  }
+  public get(
+    id: string | number,
+    query?: {
+      fields?: Array<keyof TG>;
+      join?: IJoin[];
+    }
+  ): Observable<TG> {
+    return this.getHeaders$().pipe(
+      switchMap(headers => {
+        return this.httpAdapter.get(
+          `${this.path}/${id}?${this.formatFields(query)}&${this.formatJoin(
+            query
+          )}`,
+          {
+            headers
+          }
+        );
       }),
+      map(response => response.json() as TG),
+      catchError((err: any) => {
+        this.errorHandler(err);
+        return throwError(err);
+      })
     );
   }
 
   public query(query?: TQ): Observable<INestjsxResQueryResult<TG>> {
     return this.getHeaders$().pipe(
-      switchMap((headers) => {
-        return this.httpAdapter.get(`${this.path}?${
-          this.formatFields(query)
-          }&${
-          this.formatJoin(query)
-          }&${
-          this.formatFilter(query)
-          }&${
-          this.formatOr(query)
-          }&${
-          this.formatSort(query)
-          }`, {
+      switchMap(headers => {
+        return this.httpAdapter.get(
+          `${this.path}?${this.formatFields(query)}&${this.formatJoin(
+            query
+          )}&${this.formatFilter(query)}&${this.formatOr(
+            query
+          )}&${this.formatSort(query)}`,
+          {
             params: {
-              limit: query.limit ? query.limit : '',
-              offset: query.offset ? query.offset : '',
-              page: query.page ? query.page : '',
-              cache: query.cache ? query.cache : ''
+              limit: query.limit ? query.limit : "",
+              offset: query.offset ? query.offset : "",
+              page: query.page ? query.page : "",
+              cache: query.cache ? query.cache : ""
             },
-            headers,
-          });
+            headers
+          }
+        );
       }),
-      map((response) => response.json() as INestjsxResQueryResult<TG>),
+      map(response => response.json() as INestjsxResQueryResult<TG>),
       catchError((err: any) => {
         this.errorHandler(err);
         return throwError(err);
-      }),
+      })
     );
   }
-
 }
